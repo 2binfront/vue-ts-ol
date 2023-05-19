b7eb86bb761a0b354d2643b0a7d342228b9f262b
2binfront <824225714@qq.com>
2023-05-19 22:56:11 +0800

    new cities

--- src/types/index.ts
+++ src/types/index.ts
@@ -36,6 +36,37 @@ const CITIES = [
   '港澳台或国外'
 ];
 
+const WCITIES = [
+  '北京市',
+  '天津市',
+  '河北省',
+  '山东省',
+  '江苏省',
+  '上海市',
+  '浙江省',
+  '福建省',
+  '广东省',
+  '海南省',
+  '香港特别行政区',
+  '澳门特别行政区',
+  '台湾省'
+];
+const MCITIES = ['山西省', '吉林省', '黑龙江省', '安徽省', '江西省', '河南省', '湖北省', '湖南省'];
+const ECITIES = [
+  '重庆市',
+  '四川省',
+  '云南省',
+  '贵州省',
+  '西藏自治区',
+  '陕西省',
+  '甘肃省',
+  '青海省',
+  '新疆维吾尔自治区',
+  '宁夏回族自治区',
+  '内蒙古自治区',
+  '广西壮族自治区'
+];
+
 const CENTERS = [
   { name: '北京市', center: '116.407394,39.904211' },
   { name: '天津市', center: '117.200983,39.084158' },
@@ -496,7 +527,7 @@ interface FlowRoute {
   };
 }
 
-export { CITIES, CENTERS, ALLCENTER };
+export { CITIES, CENTERS, ALLCENTER, WCITIES, MCITIES, ECITIES };
 export type { FlowRoute };
 
 // let allCityCenter = [];
